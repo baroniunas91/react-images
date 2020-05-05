dac31956058b14a73922badbe7a19c0ef14050d3
Edgaras <baroniunas@gmail.com>
2020-05-05 15:00:19 +0300

    Updates

---
+++ assets/scripts/custom.js
@@ -0,0 +1,7 @@
+"use strict";
+$(document).ready(function () {
+  $('[data-fancybox="gallery"]').fancybox({
+    loop: true,
+    animationEffect: "zoom",
+  });
+});
---
+++ precache-manifest.3065269183429c25b0c6a4d331090bcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cba55e0c0910b366a45fb220e5a4f16",
+    "url": "/react-images/index.html"
+  },
+  {
+    "revision": "8407c788ecc764abe18c",
+    "url": "/react-images/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "2a0f7da8fb775db7aa75",
+    "url": "/react-images/static/css/main.fceabcfe.chunk.css"
+  },
+  {
+    "revision": "8407c788ecc764abe18c",
+    "url": "/react-images/static/js/2.fed6b0ad.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-images/static/js/2.fed6b0ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0f7da8fb775db7aa75",
+    "url": "/react-images/static/js/main.00643158.chunk.js"
+  },
+  {
+    "revision": "f49b647dd2fa3dc0a757",
+    "url": "/react-images/static/js/runtime-main.252cc454.js"
+  }
+]);
